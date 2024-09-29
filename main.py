b7d6cbe4bf855ed651c540a95bf887335a10f117
Vallou2024 <louanne.chopin@gmail.com>
2024-09-29 18:33:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ class Jeu:
     self.k = rd.randint(0, m)
     self.n = n
 
+  def test(self, k):
+    """
+    Compare le nombre deviné avec le nombre à deviner.
+
+    Parameters:
+        guess (int): Le nombre à deviner.
+
+    Returns:
+        bool: True si les nombres sont égaux, False sinon.
+    """
+    if k != self.k:
+      if k < self.k:
+        print("Trop Petit")
+      if k > self.k:
+        print("Trop Grand")
+      return False
+    else:
+      print("Bravo tu as gagne")
+    return True
+
+
+
 
 if __name__ == '__main__':
   import doctest
